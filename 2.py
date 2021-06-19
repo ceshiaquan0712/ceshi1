375ac6e4eb5e25d6f32c00df4cb134520ef3f503
ceshiaquan0712 <1176900157@qq.com>
2021-06-19 14:20:09 +0800

    提交测试用例

---
+++ 2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Author:Administrator
+# @File :2.py
+# @Software :PyCharm
+# @Time :2021/6/10 0010 18:24
+# @company:湖南省零檬信息技术有限公司
+# print ("""
+# 请输入姓名：musne
+# 请输入年龄：18
+# 请输入性别：nan
+# ************************
+# {0}:musne
+# {2}:nan
+# {1}:18
+# ************************
+# """.format("姓名","年龄","性别"))
+str1 = 'python hello aaa 123123aabb'
+#1/ print (str1[0:6])
+#2/ str3= 'o a' in str1
+# print(str3)
+# str4= 'he' in str1
+# print(str4)
+# str5=  'ab' in str1
+# print(str5)
+
+# 3/str2=str1.replace('python','lemon')
+# print(str2)
+# 4/str2=str1.index("aaa")
+# print(str2)
+
---
+++ 3.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# @Author:Administrator
+# @File :3.py
+# @Software :PyCharm
+# @Time :2021/6/10 0010 18:50
+# @company:湖南省零檬信息技术有限公司
+# list_1=[]
+# print(type(list_1))
+# list_2=[1,2,3,4,1,2,3.111,'scb21最棒！',True,[1,2,3]]
+# print(list_2)
+# print(list_2[1])
+# print(list_2[1:6:1])
+# print(list_2[9][1])
+# list_2.append('cbq')
+# print(list_2)
+# list_2.insert(2,'cbq')
+# # # print(list_2)
+# list_2.extend([6,7,8,9])
+# print(list_2)
+# list_2=[1,2,3,4,1,2,3.111,'scb21最棒！',True,[1,2,3]]
+# list_2.pop()
+# print(list_2)
+# list_2.pop(6)
+# print(list_2)
+# # # list_2.remove('scb21最棒！')
+# # # # print(list_2)
+# # # list_2.remove(1)
+# # # print(list_2)
+# list_2=[1,2,3,4,1,2,3.111,'scb21最棒！',True,[1,2,3]]
+# list_2[4]='cbq'
+# print(list_2)
+# money =int(input('请输入你的工资：'))
+# if money>=100:
+#     print('加油')
+# elif money>=50:
+#     print('加油把')
+# else:
+# #     print('爱你')
+# a=[1,2,'6','summer']
+# b='i' in a
+# print(b)
+# dict_1={"class_id":45,'num':20}
+# if dict_1['num']>5:
+#     print(dict_1['num'])
+# else:
+#     print('上课人数不大于5')
+list3 = ['sunny','HLH', '家具', '维尼', '蒲公英的梦', '王小柒']
+
+dict_1={'name':'sunny','sex':'nan','age':18,'city':'山西'}
+dict_2={'name':'HLH','sex':'nan','age':17,'city':'山东'}
+dict_3={'name':'家具','sex':'nan','age':17,'city':'陕西'}
+dict_4={'name':'维尼','sex':'nan','age':17,'city':'陕西'}
+dict_5={'name':'蒲公英的梦','sex':'women','age':17,'city':'陕西'}
+dict_6={'name':'王小柒','sex':'women','age':17,'city':'陕西'}
+list_1=['''dict_1,dict_2,dict_3,dict_4,dict_5,dict_6''']
+print(list_1)
